2672a635171cdf2f24eaf9ee4393f228dff3a7f2
ernie-blockswap <94845412+ernie-blockswap@users.noreply.github.com>
2022-12-12 12:37:57 +0300

    fix: removed "save task" btn[C

--- script/script.js
+++ script/script.js
@@ -4,18 +4,25 @@
 let taskForm = document.querySelector('.task-form'),
     taskInput = document.querySelector('.task-input'),
     taskBody = document.querySelector('.task-list'),
+    idCounter = 0,
     taskObjArr;
 
-    initTodoList();
+initTodoList();
 
 //functions
+function getId() {
+    return idCounter++
+}
 function initTodoList() {
     if (localStorage.getItem('todoList') == null) {
         taskObjArr = [];
         return;
     }
     taskObjArr = JSON.parse(localStorage.getItem('todoList'));
-    taskObjArr.forEach((item) => taskBody.innerHTML += `<li class='task-item'><label><input type="checkbox"><span>${item.task}</span></label></li>`);
+    taskObjArr.forEach((item) => {
+        let checkedVariable = item.checked ? 'checked' : '';
+        taskBody.innerHTML += `<li class='task-item'><label><input type="checkbox" ${checkedVariable}><span>${item.task}</span></label></li>`
+    });
     console.log(taskObjArr)
 }
 function checkValidationTask(currentTask) {
@@ -30,14 +37,14 @@ taskForm.addEventListener('click', (event) => {
         if (checkValidationTask(currentTask)) return;
         let taskObj = {
             task: currentTask,
+            checked: false,
         }
         taskObjArr.push(taskObj);
         taskBody.innerHTML += `<li class='task-item'><label><input type="checkbox"><span>${taskObj.task}</span></label></li>`
         taskInput.value = '';
-    }
-    if (event.target.classList.contains('save-task')) {
         localStorage.setItem('todoList', JSON.stringify(taskObjArr));
     }
+
     if (event.target.classList.contains('clear-task-list')) {
         localStorage.removeItem('todoList');
         taskObjArr = [];
@@ -46,3 +53,12 @@ taskForm.addEventListener('click', (event) => {
     }
 })
 
+taskBody.addEventListener('change', (event) => {
+    let currentTask = event.target.nextElementSibling.innerText,
+        taskObj = taskObjArr.find(item => {
+            return item.task === currentTask;
+        });
+    taskObj.checked = !taskObj.checked;
+    localStorage.setItem('todoList', JSON.stringify(taskObjArr));
+    console.log(taskObj)
+})
